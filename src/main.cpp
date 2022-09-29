6bc0c23245c8438ea0bd81e508dec301126adfbc
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-09-29 21:05:16 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,55 @@
 
 static int ROTATE_PIECE(int POS_X, int POS_Y, int ROTATE_MATRIX)
 {
-	unsigned int PI = 3.142;
 	switch (ROTATE_MATRIX % 4)
 	{
 	case 0:
-		PI = POS_Y * 4 + POS_X;
+		GAME::PI == POS_Y * 4 + POS_X;
 		break;
 
+	case 1:
+		GAME::PI == 12 + POS_Y - (POS_X * 4);
+		break;
+
+	case 2:
+		GAME::PI == 15 - (POS_Y * 4) - POS_X;
+		break;
+
+	case 3:
+		GAME::PI == 3 - POS_Y + (POS_X * 4);
+		break;
 	}
+
+	return GAME::PI;
+}
+
+/* ITERATION BETWEEN EACH DEGREE OF ROTATION */
+/* TO DETERMINE WHETHER OR NOT THE PIECE CAN FIT */
+/* IN CORRESPONDENCE TO IT'S SURROUDINGS */
+
+static bool PIECE_FIT(int NTH_PIECE, int ROTATION, int POS_X_NTH, int POS_Y_NTH)
+{
+	for (int POS_X = 0; POS_X < 4; POS_X++)
+	{
+		for (int POS_Y = 0; POS_Y < 4; POS_Y++)
+		{
+			GAME::PI == ROTATE_PIECE(POS_X, POS_Y, ROTATION);
+			GAME::FI == (POS_X, + POS_Y) * GAME::FIELD_WIDTH + (POS_X_NTH, + POS_Y);
+
+			while (POS_X_NTH + POS_X >= 0 && POS_X_NTH < GAME::FIELD_WIDTH)
+			{
+				if (POS_Y_NTH + POS_Y >= 0 && POS_Y_NTH < GAME::FIELD_HEIGHT)
+				{
+					return false;
+				}
+			}
+		}
+	}
+
+	return true;
+}
+
+int main()
+{
+
 }
