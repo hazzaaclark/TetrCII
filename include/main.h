f115ed41139a07f280a5c8ef83d30a82815037cd
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-09-29 21:51:31 +0100

    Update main.h

--- include/main.h
+++ include/main.h
@@ -8,23 +8,26 @@
 #include <Windows.h>
 
 using std::vector;
+using std::wstring;
 
 typedef struct SCREEN_VARIABLES
 {
+	static const wstring PIECE[7];
 	static const int WIDTH = 100;
 	static const int HEIGHT = 50;
-	unsigned char* POINTER_FILED = nullptr;
+	static const char* POINTER_FIELD;
 
 } SCREEN;
 
 typedef struct GAME_LOGIC
 {
 	static const int PI = 3.142;
-	int FIELD_WIDTH = 12;
-	int FIELD_HEIGHT = 18;
+	static const int FI = 0;
+	static const int FIELD_WIDTH = 12;
+	static const int FIELD_HEIGHT = 18;
 	int CURRENT_PIECE = 0;
 	int CURRENT_ROTATION = 0;
-	int CURRENT_X_POS = FIELD_WIDTH /= 2;
+	int CURRENT_X_POS;
 	int CURRENT_Y_POS;
 	int SPEED = 20;
 	int PIECE_COUNT = 0;
@@ -35,5 +38,6 @@ typedef struct GAME_LOGIC
 	vector<int*> VECTOR_LINES;
 
 } GAME;
+
 
 #endif 
