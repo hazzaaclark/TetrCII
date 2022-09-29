6356b8b9e60e1fbb5d4608e84f9e610e0d59cd67
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-09-29 16:14:07 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include "main.h"
 
-/*  */
+/* SWITCH AND CASE INTERATION THAT RETURNS AN INT VALUE */
+/* AT THE END OF IT'S EXECUTION */
+/* THIS IS AN ITERABLE LOOP THAT DETERMINES THE ROTATION OF THE PIECE */
 
-int ROTATE_PIECE(int POS_X, int POS_Y, int ROTATE_MATRIX)
+static int ROTATE_PIECE(int POS_X, int POS_Y, int ROTATE_MATRIX)
 {
-	const int PI = 3.142;
+	unsigned int PI = 3.142;
 	switch (ROTATE_MATRIX % 4)
 	{
 	case 0:
+		PI = POS_Y * 4 + POS_X;
+		break;
 
 	}
 }
