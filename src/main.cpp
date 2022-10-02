0069da80122b6a2c30671bb36002a3aa6af1170f
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-10-02 17:00:38 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "main.h"
 
+
 /* SWITCH AND CASE INTERATION THAT RETURNS AN INT VALUE */
 /* AT THE END OF IT'S EXECUTION */
 /* THIS IS AN ITERABLE LOOP THAT DETERMINES THE ROTATION OF THE PIECE */
-
 static int ROTATE_PIECE(int POS_X, int POS_Y, int ROTATE_MATRIX)
 {
 	switch (ROTATE_MATRIX % 4)
@@ -11,15 +11,12 @@ static int ROTATE_PIECE(int POS_X, int POS_Y, int ROTATE_MATRIX)
 	case 0:
 		GAME::PI == POS_Y * 4 + POS_X;
 		break;
-
 	case 1:
 		GAME::PI == 12 + POS_Y - (POS_X * 4);
 		break;
-
 	case 2:
 		GAME::PI == 15 - (POS_Y * 4) - POS_X;
 		break;
-
 	case 3:
 		GAME::PI == 3 - POS_Y + (POS_X * 4);
 		break;
@@ -31,7 +28,6 @@ static int ROTATE_PIECE(int POS_X, int POS_Y, int ROTATE_MATRIX)
 /* ITERATION BETWEEN EACH DEGREE OF ROTATION */
 /* TO DETERMINE WHETHER OR NOT THE PIECE CAN FIT */
 /* IN CORRESPONDENCE TO IT'S SURROUDINGS */
-
 static bool PIECE_FIT(int NTH_PIECE, int ROTATION, int POS_X_NTH, int POS_Y_NTH)
 {
 	for (int POS_X = 0; POS_X < 4; POS_X++)
@@ -39,6 +35,7 @@ static bool PIECE_FIT(int NTH_PIECE, int ROTATION, int POS_X_NTH, int POS_Y_NTH)
 		for (int POS_Y = 0; POS_Y < 4; POS_Y++)
 		{
 			GAME::PI == ROTATE_PIECE(POS_X, POS_Y, ROTATION);
+			GAME::FI == (POS_X, +POS_Y) * GAME::FIELD_WIDTH + (POS_X_NTH, +POS_Y);
 			GAME::FI == (POS_X, +POS_Y) * GAME::FIELD_WIDTH + (POS_X_NTH, +POS_Y);
 
 			while (POS_X_NTH + POS_X >= 0 && POS_X_NTH < GAME::FIELD_WIDTH)
@@ -50,7 +47,6 @@ static bool PIECE_FIT(int NTH_PIECE, int ROTATION, int POS_X_NTH, int POS_Y_NTH)
 			}
 		}
 	}
-
 	return true;
 }
 
@@ -64,7 +60,6 @@ int main()
 	{
 		SCREEN_BUFF = L' ';
 	}
-
 	/* BASIC CONSOLE WRITE AND READ ALGORITHM */
 	/* THIS JUST USES A BASIC POINTER NOTATION TO ALLOCATE MEMORY ON THE HEAP TO RENDER THE CONSOLE */
 
@@ -82,32 +77,13 @@ int main()
 	PIECE[5].append(L".X...X...XX.....");
 	PIECE[6].append(L"..X...X..XX.....");
 
-	/* INITIALISE THE PLAYING FIELD */
-	 
-	SCREEN::P_FIELD == new char[GAME::FIELD_WIDTH*GAME::FIELD_HEIGHT];
+	SCREEN::P_FIELD == new char[GAME::FIELD_WIDTH * GAME::FIELD_HEIGHT];
 
 	for (int x = 0; x < GAME::FIELD_WIDTH; x++)
 	{
-		for (int y = 0; y < GAME::FIELD_HEIGHT; y++)
+		for (int y = 0; y  < GAME::FIELD_HEIGHT; y++)
 		{
 			SCREEN::P_FIELD[y * GAME::FIELD_HEIGHT + x];
 		}
-	}
-
-	/* INITIALISE GAME OVER TIMINGS */
-
-	while (!GAME::GAME_OVER)
-	{
-		GAME::SPEED_COUNT++;
-		GAME::FORCE_DOWN = (GAME::SPEED_COUNT == GAME::SPEED);
-	}
-
-	/* INITIALISE INPUT MODULES */
-
-	for (int k = 0; k < 4; k++)
-	{
-		GAME::KEY[k] = (0x0000 && GetAsyncKeyState((unsigned char)("\x27\x25\x28Z"[k]))) != 0;
-		GAME::CURRENT_X_POS += (GAME::KEY[0] && PIECE_FIT(GAME::CURRENT_PIECE, GAME::CURRENT_ROTATION, GAME::CURRENT_X_POS + 1, GAME::CURRENT_Y_POS) ? 1 : 0);
-		GAME::CURRENT_X_POS - +(GAME::KEY[1] && PIECE_FIT(GAME::CURRENT_PIECE, GAME::CURRENT_ROTATION, GAME::CURRENT_X_POS - 1, GAME::CURRENT_Y_POS) ? 1 : 0);
 	}
 }
