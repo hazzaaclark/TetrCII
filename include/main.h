d7781bd697774fb8f201ef8cffb039be691ff2db
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-10-02 17:00:49 +0100

    Update main.h

--- include/main.h
+++ include/main.h
@@ -1,7 +1,7 @@
 #pragma once
+
 #ifndef TETRIS_MAIN_H
 #define TETIRS_MAIN_H
-
 #include <iostream>
 #include <stdio.h>
 #include <vector>
@@ -27,19 +27,18 @@ typedef struct GAME_LOGIC
 	static const int FIELD_WIDTH = 12;
 	static const int FIELD_HEIGHT = 18;
 	static int CURRENT_PIECE;
-	static int CURRENT_ROTATION = 0;
-	static int CURRENT_X_POS;
-	static int CURRENT_Y_POS;
+	int CURRENT_ROTATION = 0;
+	int CURRENT_X_POS;
+	int CURRENT_Y_POS;
 	static int SPEED;
 	static int SPEED_COUNT;
 	int PIECE_COUNT = 0;
 	int SCORE = 0;
-	static bool KEY[4];
-	static bool FORCE_DOWN;
-	bool ROTATE_HOLD();
-	bool GAME_OVER();
+	virtual bool FORCE_DOWN(VOID);
+	virtual bool ROTATE_HOLD(VOID);
+	virtual bool GAME_END(VOID);
 	vector<int*> VECTOR_LINES;
 
 } GAME;
 
-#endif 
+#endif
