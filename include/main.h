ff1d6de927efd5c5e526caab60a78a8bc4a12364
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-10-03 00:20:07 +0100

    Update main.h

--- include/main.h
+++ include/main.h
@@ -20,25 +20,4 @@ typedef struct SCREEN_VARIABLES
 
 } SCREEN;
 
-typedef struct GAME_LOGIC
-{
-	static const int PI = 3.142;
-	static const int FI = 0;
-	static const int FIELD_WIDTH = 12;
-	static const int FIELD_HEIGHT = 18;
-	static int CURRENT_PIECE;
-	int CURRENT_ROTATION = 0;
-	int CURRENT_X_POS;
-	int CURRENT_Y_POS;
-	static int SPEED;
-	static int SPEED_COUNT;
-	int PIECE_COUNT = 0;
-	int SCORE = 0;
-	virtual bool FORCE_DOWN(VOID);
-	virtual bool ROTATE_HOLD(VOID);
-	virtual bool GAME_END(VOID);
-	vector<int*> VECTOR_LINES;
-
-} GAME;
-
 #endif
