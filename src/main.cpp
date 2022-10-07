79563339a84fe42a6c1743163f64f61411d20581
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-10-07 01:23:05 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,5 @@
 #include "main.h"
 
-#define INSTRUCTIONS
-bool FORCE_DOWN;
-bool ROTATE_HOLD;
-bool GAME_END;
-
-#define GAME
-unsigned char* P_FIELD;
-static int PI = 3.142;
-static const int FI = 0;
-static const int FIELD_WIDTH = 12;
-static const int FIELD_HEIGHT = 18;
-static int CURRENT_PIECE;
-static int CURRENT_ROTATION;
-static int CURRENT_X_POS;
-static int CURRENT_Y_POS;
-static int SPEED;
-static int SPEED_COUNT;
-int PIECE_COUNT = 0;
-int SCORE = 0;
-static bool KEY[4];
-vector<int*> VECTOR_LINES;
-
 /* SWITCH AND CASE INTERATION THAT RETURNS AN INT VALUE */
 /* AT THE END OF IT'S EXECUTION */
 /* THIS IS AN ITERABLE LOOP THAT DETERMINES THE ROTATION OF THE PIECE */
